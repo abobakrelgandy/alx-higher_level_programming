4a0ca68f1b1bf14fd74d7cbcca3b7d95a1410c26
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-11-27 17:22:36 -0800

    one

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 98
-name = "hanin"
-age = 19
-print("Hello, {}! You're {} years old.".format(name, age))
-"Hello, hanin! You're 19 years old."
+print("{:d} Battery street".formate(number))

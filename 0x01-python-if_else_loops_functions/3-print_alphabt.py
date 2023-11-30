5edfdab738eb019c7b8a3c15d7f8df8e2b5f2e02
abobakrelgandy <abobakrelgandy338@gmail.com>
2023-11-29 17:22:22 -0800

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']))
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
